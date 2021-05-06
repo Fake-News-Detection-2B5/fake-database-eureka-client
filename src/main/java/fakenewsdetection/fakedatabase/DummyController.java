fb090acf949843ee196e6e70734db7ce67746468
adipeterca <adipeterca@gmail.com>
2021-05-06 10:23:53 +0300

    dummy

--- src/main/java/fakenewsdetection/fakedatabase/DummyController.java
+++ src/main/java/fakenewsdetection/fakedatabase/DummyController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "lol")
+@RequestMapping(path = "/lol")
 public class DummyController {
 
     @GetMapping(path = "/getDummy")
